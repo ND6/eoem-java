bd263456367429d598254ea66eb11ee0547e2920
yanpengfei <noch1217@qq.com>
2023-02-27 15:06:54 +0800

    add switch test

---
+++ javase/src/main/java/org/eoem/lang/HowToSwitch.java
@@ -0,0 +1,18 @@
+package org.eoem.lang;
+
+public class HowToSwitch {
+    public static void main(String[] args) {
+        int a = 3;
+        switch (a) {
+            case 1:
+                System.out.println("### 1");
+            case 2:
+                System.out.println("### 2");
+            case 3:
+                ;
+            case 4:
+                System.out.println("### 4");
+        }
+        
+    }
+}
