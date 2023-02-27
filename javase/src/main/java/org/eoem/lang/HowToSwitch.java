99484feb00844784a3fea727f6336abe66fb465e
yanpengfei <noch1217@qq.com>
2023-02-27 15:16:45 +0800

    switch test

--- javase/src/main/java/org/eoem/lang/HowToSwitch.java
+++ javase/src/main/java/org/eoem/lang/HowToSwitch.java
@@ -6,12 +6,15 @@ public class HowToSwitch {
         switch (a) {
             case 1:
                 System.out.println("### 1");
+                break;
             case 2:
                 System.out.println("### 2");
+                break;
             case 3:
                 ;
             case 4:
                 System.out.println("### 4");
+                break;
         }
         
     }
