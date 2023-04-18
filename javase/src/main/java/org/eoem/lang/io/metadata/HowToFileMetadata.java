0ad786bab26d1b677a358cfc767ade4e84170c93
yanpengfei <noch1217@qq.com>
2023-04-18 10:35:27 +0800

    add posixFileAttributes

--- javase/src/main/java/org/eoem/lang/io/metadata/HowToFileMetadata.java
+++ javase/src/main/java/org/eoem/lang/io/metadata/HowToFileMetadata.java
@@ -6,6 +6,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
+import java.nio.file.attribute.PosixFileAttributes;
+import java.nio.file.attribute.PosixFilePermissions;
 
 public class HowToFileMetadata {
     public static void main(String[] args) {
@@ -20,31 +22,38 @@ public class HowToFileMetadata {
             System.out.println("Files.isDirectory " + Files.isDirectory(path));
             
             System.out.println("Files.isRegularFile " + Files.isRegularFile(path));
-            
+    
             System.out.println("Files.isSymbolicLink " + Files.isSymbolicLink(path));
-            
+    
             System.out.println("Files.getLastModifiedTime " + Files.getLastModifiedTime(path));
             System.out.println("Files.getOwner " + Files.getOwner(path));
             System.out.println("Files.getAttribute " + Files.getAttribute(path, "size"));
-            
+    
             Path file = Paths.get("/test/fd1.json");
-            BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
-            
-            System.out.println("creationTime: " + attr.creationTime());
-            System.out.println("lastAccessTime: " + attr.lastAccessTime());
-            System.out.println("lastModifiedTime: " + attr.lastModifiedTime());
-            
-            System.out.println("isDirectory: " + attr.isDirectory());
-            System.out.println("isOther: " + attr.isOther());
-            System.out.println("isRegularFile: " + attr.isRegularFile());
-            System.out.println("isSymbolicLink: " + attr.isSymbolicLink());
-            System.out.println("size: " + attr.size());
-            System.out.println("attr.fileKey(): " + attr.fileKey());
-            
+            BasicFileAttributes basicFileAttributes = Files.readAttributes(file, BasicFileAttributes.class);
+    
+            System.out.println("creationTime: " + basicFileAttributes.creationTime());
+            System.out.println("lastAccessTime: " + basicFileAttributes.lastAccessTime());
+            System.out.println("lastModifiedTime: " + basicFileAttributes.lastModifiedTime());
+    
+            System.out.println("isDirectory: " + basicFileAttributes.isDirectory());
+            System.out.println("isOther: " + basicFileAttributes.isOther());
+            System.out.println("isRegularFile: " + basicFileAttributes.isRegularFile());
+            System.out.println("isSymbolicLink: " + basicFileAttributes.isSymbolicLink());
+            System.out.println("size: " + basicFileAttributes.size());
+            System.out.println("attr.fileKey(): " + basicFileAttributes.fileKey());
+    
             long currentTime = System.currentTimeMillis();
             FileTime ft = FileTime.fromMillis(currentTime);
             Files.setLastModifiedTime(file, ft);
-            
+    
+            PosixFileAttributes posixFileAttributes =
+                    Files.readAttributes(file, PosixFileAttributes.class);
+            System.out.format("posixFileAttributes:%s %s %s%n",
+                    posixFileAttributes.owner().getName(),
+                    posixFileAttributes.group().getName(),
+                    PosixFilePermissions.toString(posixFileAttributes.permissions()));
+    
         } catch (IOException e) {
             System.out.println(e);
         }
